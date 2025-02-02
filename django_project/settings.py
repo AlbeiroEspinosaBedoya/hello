1b22787ca68522f6d15a0f0c5fd0c04ca410f22c
Albeiro Espinosa Bedoya <aespinos@unal.edu.co>
2025-02-02 13:16:20 -0500

    Commit_2025020201

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ufsf^&dn2s=7-s0ok=&g$aya^=_t_yg(rkzv#5+))&pb&tl&2v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
